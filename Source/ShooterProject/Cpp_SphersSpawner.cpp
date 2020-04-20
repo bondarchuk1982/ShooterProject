8e1444c644aa36f7c969c00b79a9776e9fdffda3
Sergey Bondarchuk <bondarchuk.sa.1982@gmail.com>
2020-04-20 20:24:36 +0300

    Refactored code and added comments in code

--- Source/ShooterProject/Cpp_SphersSpawner.cpp
+++ Source/ShooterProject/Cpp_SphersSpawner.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#include <ctime>
-#include <algorithm>
-#include <string>
 
 #include "Cpp_SphersSpawner.h"
+
+#include <ctime>
+
 #include "Math/UnrealMathUtility.h"
 
 // Sets default values
@@ -23,56 +23,55 @@ void ACpp_SphersSpawner::BeginPlay()
 void ACpp_SphersSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	// Checking marked spheres for destroing
+	m_spheres.RemoveAll([=](auto sphere) {
+			if (sphere->IsActorBeingDestroyed()) { // If marked for destroy, remove from container
+				auto dist = FVector::Dist(m_spawnPoint, sphere->GetActorLocation()); // The distance between the spawn point and the center of the sphere
 
-	m_spheres.erase(std::remove_if(m_spheres.begin(), m_spheres.end(),
-		[=](auto sphere) {
-			if (sphere->IsActorBeingDestroyed()) {
-				auto dist = FVector::Dist(m_spawnPoint, sphere->GetActorLocation());
-
-				if (dist <= m_spawnRadius) {
-					++totalScore;
+				if (dist <= m_spawnRadiusForNextLevel) { // If the sphere to be destroyed is in radius to pass the level, then we increase the score
+					++totalScore; // Increment score
 				}
 
 				return true;
 			}
 
 			return false;
-		}), m_spheres.end());
+		});
 
+	// Check the score for passing the level
 	CheckTotalLevel();
 }
 
 void ACpp_SphersSpawner::OnSpawn()
 {
-	m_spawnPoint = CharacterPoint;
-	GenerateNewSpheres(m_spawnCountSpheres, 2000, 80);
+	m_spawnPoint = CharacterPoint; // Set the spawn point at the player’s point
+	GenerateNewSpheres(m_spawnCountSpheres, m_startSpawnRadius); // Generate spheres for start game
 }
 
 void ACpp_SphersSpawner::GenerateNewSpheres(float newCount, float newRadius, float minDistance)
 {
-	std::srand(unsigned(std::time(0)));
+	std::srand(unsigned(std::time(0))); // For std::rand()
 
-	auto radius = static_cast<int>(newRadius);
-	auto count = static_cast<int>(newCount);
+	for (int32 i = 0; i < newCount;) {
+		float totalRadius = i < 10 ? m_spawnRadiusForNextLevel : newRadius; // Guarantee the presence of 10 spheres in the radius for passing the level
 
-	for (int32 i = 0; i < count; ++i) {
-		int totalRadius = i < 10 ? m_spawnRadius : radius;
-
-		auto random2DPoint = FMath::RandPointInCircle(totalRadius);
+		auto random2DPoint = FMath::RandPointInCircle(totalRadius); // Generate a new random 2D position of the sphere in radius along the XY axes
+		// Add the generated position to the FVector and generate additionally along the Z axis
 		auto newPoint = FVector(random2DPoint.X, random2DPoint.Y, std::rand() % 240 + 10);
-		auto location = m_spawnPoint + newPoint;
-		bool isNewPoint = true;
+		auto location = m_spawnPoint + newPoint; // Set a new point for the appearance of a sphere
+		bool isNewPoint = true; // Correct New Position Flag
 
 		for (auto const &sphere : m_spheres) {
-			auto dist = FVector::Dist(newPoint, sphere->GetActorLocation());
+			auto dist = FVector::Dist(newPoint, sphere->GetActorLocation()); // Check for violation of the minimum distance between the spheres
 
-			if (dist < minDistance) {
+			if (dist < minDistance) { // If the minimum distance is violated, switch the flag to the incorrect position
 				isNewPoint = false;
 			}
 		}
 
-		if (isNewPoint) {
+		if (isNewPoint) { // If the new position is correct, generate a new sphere at this point
 			SpawnNewSphere(location);
+			++i; // Increment "i" for next iteration
 		}
 	}
 }
@@ -80,22 +79,26 @@ void ACpp_SphersSpawner::GenerateNewSpheres(float newCount, float newRadius, flo
 void ACpp_SphersSpawner::SpawnNewSphere(FVector location)
 {
 	if (SphereToSpawn) {
-		UWorld* const World = GetWorld();
+		UWorld* const World = GetWorld(); // Get the world
 
 		if (World) {
-			auto sphere = World->SpawnActor(SphereToSpawn, &location, &m_rotation);
-			m_spheres.push_back(sphere);
+			auto sphere = World->SpawnActor(SphereToSpawn, &location, &m_rotation); // Generate a new sphere
+			m_spheres.Add(sphere); // Add to container
 		}
 	}
 }
 
 void ACpp_SphersSpawner::CheckTotalLevel()
 {
-	if (totalScore >= totalLevel * 10) {
-		m_spawnPoint = CharacterPoint;
-		GenerateNewSpheres(m_spawnCountSpheres + m_spawnCountSpheres * totalLevel * 0.1,
-							m_spawnRadius + m_spawnRadius * totalLevel * 0.05, 80);
+	if (totalScore >= totalLevel * 10) { // Check score exceeded or equal to the requirement for a new level
+		m_spawnPoint = CharacterPoint; // Save the current player position as a spawn point
 
-		++totalLevel;
+		// Generate new spheres with an increment of the radius and the count of spheres for the current level
+		// 0.1 - 10% in level increment for count spheres
+		// 0.05 - 5% in level increment for radius spawn
+		GenerateNewSpheres(m_spawnCountSpheres + m_spawnCountSpheres * totalLevel * 0.1,
+							m_startSpawnRadius + m_startSpawnRadius * totalLevel * 0.05);
+
+		++totalLevel; // Increment the current level
 	}
 }
--- Source/ShooterProject/Cpp_SphersSpawner.h
+++ Source/ShooterProject/Cpp_SphersSpawner.h
@@ -1,15 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #pragma once
-#include <vector>
 
 #include "CoreMinimal.h"
-#include "Runtime/Engine/Classes/GameFramework/Actor.h"
-#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
-#include "Runtime/Engine/Classes/Engine/World.h"
-#include "Runtime/Engine/Public/EngineUtils.h"
-#include "Runtime/CoreUObject/Public/UObject/Object.h"
-#include "Kismet/KismetMathLibrary.h"
-#include "Kismet/KismetSystemLibrary.h"
+#include "GameFramework/Actor.h"
+#include "UObject/Object.h"
+#include "Engine/World.h"
+#include "Engine/Public/EngineUtils.h"
+
 #include "Cpp_SphersSpawner.generated.h"
 
 UCLASS()
@@ -33,25 +30,27 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sphere spawn")
-	TSubclassOf<class AActor> SphereToSpawn;
+	TSubclassOf<class AActor> SphereToSpawn; // Actor for spawn
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sphere spawn")
-	FVector CharacterPoint;
+	FVector CharacterPoint; // Player location
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sphere spawn")
-	int32 totalScore = 0;
+	int32 totalScore = 0; // Variable for read total score 
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sphere spawn")
-	int32 totalLevel = 1;
+	int32 totalLevel = 1; // Variable for read total level
 
 private:
-	void GenerateNewSpheres(float count, float radius, float minDistance);
+	// 80 - minimum distance between spheres
+	void GenerateNewSpheres(float count, float radius, float minDistance = 80.0);
 	void SpawnNewSphere(FVector location);
 	void CheckTotalLevel();
 
-	std::vector<AActor*> m_spheres;
-	FRotator m_rotation = FRotator::ZeroRotator;
-	int32 m_spawnRadius = 1500;
-	int32 m_spawnCountSpheres = 15;
-	FVector m_spawnPoint;
+	TArray<AActor*> m_spheres; // Conteiner for all spheres
+	FRotator m_rotation = FRotator::ZeroRotator; // For spheres not need rotation, so using ZeroRotator
+	int32 m_startSpawnRadius = 2000; // Maximum radius for start spawn spheres
+	int32 m_spawnRadiusForNextLevel = 1500; // Radiuse for passing the level
+	int32 m_spawnCountSpheres = 15; // Count spheres for start game
+	FVector m_spawnPoint; // The position around which spheres are spawned
 };
